8ab834deb86522bcc4eb22e79d97c7febe1a50c9
SuminOk1205 <suminok.sci@gmail.com>
2026-05-28 23:38:35 +0900

    feat(UI): Add Game Guide UI
    - Game Guide Popup
    (Popup_Game_Guide(Prefab) / BlinkingText.cs)
    - Added a blinking effect to the start text.

---
+++ Assets/_Project/Scripts/UI/BlinkingText.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class BlinkingText : MonoBehaviour
+{
+    [Header("UI Settings")]
+    [SerializeField] private TextMeshProUGUI targetText;
+    [SerializeField] private GameObject controlsPanel;
+
+    [Header("Blinking Settings")]
+    [SerializeField] private float blinkInterval = 0.5f;
+
+    private bool isStarted = false;
+
+    void Start()
+    {
+        if (targetText == null)
+        {
+            Debug.LogError("Target Text is not assigned.");
+            return;
+        }
+        if (targetText != null)
+        {
+            StartCoroutine(BlinkText());
+        }
+    }
+
+    void Update()
+    {
+        if (isStarted) return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartGame();
+        }
+    }
+
+    private void StartGame()
+    {
+        isStarted = true;
+        StopAllCoroutines();
+        targetText.alpha = 1f;
+
+        if (controlsPanel != null)
+        {
+            Debug.Log("Hiding controls panel.");
+            controlsPanel.SetActive(false);
+        }
+        
+    }  
+
+    private IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            // Fade Out
+            float time = 0f;
+            while (time < blinkInterval)
+            {
+                time += Time.deltaTime;
+                float percentage = time / blinkInterval;
+                targetText.alpha = Mathf.Lerp(1f, 0f, percentage);
+                yield return null;
+            }
+            targetText.alpha = 0f;
+
+            // Fade In
+            time = 0f;
+            while (time < blinkInterval)
+            {                
+                time += Time.deltaTime;
+                float percentage = time / blinkInterval;
+                targetText.alpha = Mathf.Lerp(0f, 1f, percentage);
+                yield return null;
+            }
+            targetText.alpha = 1f;
+        }
+    }
+}
